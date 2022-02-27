7db66203df28b08f4ea03781fd211f4f84b8b82b
syumpei1997 <syumpei1997@gmail.com>
2022-02-27 23:54:37 +0900

    マイグレーションファイル修正

---
+++ db/migrate/20220204124533_tmp.rb
@@ -0,0 +1,4 @@
+class Tmp < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_111347) do
+ActiveRecord::Schema.define(version: 2022_02_21_124121) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image_name", null: false
+    t.string "material", null: false
+    t.string "quantity", null: false
+    t.text "process", null: false
+    t.integer "ganre_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_02_03_111347) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
